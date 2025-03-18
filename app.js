88a6a83ef64ae48dd1799be2d3b01b3fb09c145b
SaleenaAhuja20 <saleenaahuja038@gmail.com>
2025-03-18 19:54:48 +0500

    .

--- app.js
+++ app.js
@@ -2,3 +2,14 @@ let boxes = document.querySelectorAll(".box"); // access boxes
 let resetBtn = document.querySelector(".Reset");
 
 let turnO = true; // player O
+
+const winPatterns = [
+    [0,1,2],
+    [0.3,6],
+    [0,4,8],
+    [1,4,7],
+    [2,4,6],
+    [2,5,8],
+    [3.4,5],
+    [6,7,8]
+];
