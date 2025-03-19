2c30c0b00eba56e88501eb59ce7367703d53d75c
SaleenaAhuja20 <saleenaahuja038@gmail.com>
2025-03-19 20:34:11 +0500

    Update app.js

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ let turnO = true; // player O
 
 const winPatterns = [
     [0,1,2],
-    [0.3,6],
+    [0,3,6],
     [0,4,8],
     [1,4,7],
     [2,4,6],
     [2,5,8],
-    [3.4,5],
+    [3,4,5],
     [6,7,8]
 ];
 
@@ -31,11 +31,15 @@ boxes.forEach((box) => {
 });
 
 const checkWinner = () => {
-  for(pattern of winPatterns){
-    
-}
-}
+  for(let pattern of winPatterns){
+    let pos1Val = boxes[pattern[0]].innerText;
+    let pos2Val = boxes[pattern[1]].innerText;
+    let pos3Val = boxes[pattern[2]].innerText;
 
-resetBtn.addEventListener("click", () =>{
-    boxes.innerText = " ";
-})
+    if(pos1Val != "" && pos2Val != "" && pos3Val != ""){
+        if(pos1Val === pos2Val && pos2Val === pos3Val){
+           console.log("winner", pos1Val);
+        }
+    }
+}
+};
